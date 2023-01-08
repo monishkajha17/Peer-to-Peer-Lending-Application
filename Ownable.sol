2dba2a3bad8f3485f50e644df27ad6271d3e36a8
Monishka Jha <jhamonishka@gmail.com>
2023-01-08 15:28:28 +0530

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,1 +1,19 @@
+pragma solidity ^0.4.18;
 
+
+contract Ownable {
+    address public owner;
+
+    event LogOwnershipTransfered(address indexed _currentOwner, address indexed _newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    
+}
